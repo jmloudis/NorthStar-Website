ed6fe49a45cce5aec492d75215624fe9a1688864
jordan <16jloudis@gmail.com>
2021-12-12 14:01:02 -0500

    This is a wesbite that I created for a small business called NorthStar Satellite, It is fully functionally and was created with HTML, CSS and JS. It is both mobile and desktop friendly

---
+++ from-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@northstarsatellites.com';
+
+$email_subject = "New Inquiry from Website";
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
+
+$to = 'jmloudis@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_from,$headers);
+
+header("Location: contact.html")
+?>
